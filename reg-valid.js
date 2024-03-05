fd18e2e60d37dba3f56353895ad917abe03abf8f
meghhana <meghana16303@gmail.com>
2024-03-05 15:37:30 +0530

    5-03

---
+++ reg-valid.js
@@ -0,0 +1,8 @@
+function formValidation()
+{
+    var uid= document.registraion.uname;
+    var passid=document.registraion.password;
+    var emailid= document.registration.email;
+    var phonenum = document.registration.phone;
+    
+}
